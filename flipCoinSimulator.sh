d2b385a5563f6e066999608f167c574a2f291656
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-05 13:25:55 +0530

    Use Case 2 - Iterating multiple times to check whether Heads or Tails occured 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,15 +4,43 @@ echo "Welcome to Flip Coin Simulation Problem"
 
 #Use Case 1 (Flipping a Coin to Display Heads or Tails as winner)
 
-flip_check=$((RANDOM%2))
+#flip_check=$((RANDOM%2))
+#
+#isHeads=1
+#
+#echo "Flip Value: "$flip_check
+#
+#if [ $flip_check == $isHeads ]
+#then
+#	echo "So, Heads is Winner"
+#else
+#	echo "So, Tails is Winner"
+#fi
+
+
+
+
+
+# Use Case 2 ( Iterating multiple times to count occurrence of "Heads" and "Tails")
 
 isHeads=1
+isTails=0
 
-echo "Flip Value: "$flip_check
+count_heads=0
+count_tails=0
 
-if [ $flip_check == $isHeads ]
-then
-	echo "So, Heads is Winner"
-else
-	echo "So, Tails is Winner"
-fi
+for((i=1;i<=20;i++))
+do
+	flip_check=$((RANDOM%2))
+
+	if [ $flip_check -eq $isHeads ]
+	then
+		count_heads=$((count_heads + 1))
+	elif [ $flip_check -eq $isTails ]
+	then
+                count_tails=$((count_tails + 1))
+	fi
+done
+
+echo "Heads occured $count_heads times"
+echo "Tails occured $count_tails times"
