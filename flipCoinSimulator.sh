2503e4e1af415496f4fb692fe90e1d796a40a625
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-05 14:20:05 +0530

    Use Case 4 - Iterating multiple times till difference of occurrence of Heads or Tails is 2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -53,7 +53,45 @@ echo "Welcome to Flip Coin Simulation Problem"
 
 #Use Case 3 (Iterating multiple times to check whether "Heads" or "Tails" occured 21 times)
 
-echo "           "
+#echo "           "
+#
+#isHeads=1
+#isTails=0
+#
+#count_heads=0
+#count_tails=0
+#
+#win_diff=0
+#
+#for((i=1;i<=40;i++))
+#do
+#        flip_check=$((RANDOM%2))
+#
+#        if [ $flip_check -eq $isHeads ]
+#        then
+#                count_heads=$((count_heads + 1))
+#        elif [ $flip_check -eq $isTails ]
+#        then
+#                count_tails=$((count_tails + 1))
+#        fi
+#
+#	if [ $count_heads -eq 21 ]
+#	then
+#		break
+#	elif [ $count_tails -eq 21 ]
+#        then
+#		break
+#	fi
+#done
+#
+#echo "Heads occured $count_heads times"
+#echo "Tails occured $count_tails times"
+
+
+
+
+
+#Use Case 4 (Iterating multiple times till difference of occurrence of "Heads" or "Tails" is 2)
 
 isHeads=1
 isTails=0
@@ -75,13 +113,23 @@ do
                 count_tails=$((count_tails + 1))
         fi
 
-	if [ $count_heads -eq 21 ]
-	then
+       	if [ $count_heads -eq 21 ]
+       	then
+               	while [ $win_diff -ne 2 ]
+		do
+			count_tails=$((count_tails + 1))
+			win_diff=$((count_heads-count_tails))
+		done
 		break
-	elif [ $count_tails -eq 21 ]
-        then
+       	elif [ $count_tails -eq 21 ]
+       	then
+                while [ $win_diff -ne 2 ]
+                do
+                        count_heads=$((count_heads + 1))
+			win_diff=$((count_tails-count_heads))
+		done
 		break
-	fi
+        fi
 done
 
 echo "Heads occured $count_heads times"
