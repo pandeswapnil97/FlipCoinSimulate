2eea2dc5819855bdac23c05a438be9ab90001f30
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-05 13:22:34 +0530

    Use Case 1 - Flipping a Coin to Display Heads or Tails as winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -x
 
 echo "Welcome to Flip Coin Simulation Problem"
+
+#Use Case 1 (Flipping a Coin to Display Heads or Tails as winner)
+
+flip_check=$((RANDOM%2))
+
+isHeads=1
+
+echo "Flip Value: "$flip_check
+
+if [ $flip_check == $isHeads ]
+then
+	echo "So, Heads is Winner"
+else
+	echo "So, Tails is Winner"
+fi
